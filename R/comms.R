f741f04170a341169cca99e2c6efd33d8bca7116
Conor <conorsg@gmail.com>
2014-11-13 12:38:45 -0600

    some pretty output

---
+++ R/comms.R
@@ -0,0 +1,26 @@
+#spits some stuff out at the end of main.R's execution
+
+init_comms <- function() {
+  #
+  #
+
+  flag <- "\n\nFINISHED\n\n
+|* * * * * * * * * * OOOOOOOOOOOOOOOOOOOOOOOOO|
+| * * * * * * * * *  :::::::::::::::::::::::::|
+|* * * * * * * * * * OOOOOOOOOOOOOOOOOOOOOOOOO|
+| * * * * * * * * *  :::::::::::::::::::::::::|
+|* * * * * * * * * * OOOOOOOOOOOOOOOOOOOOOOOOO|
+| * * * * * * * * *  ::::::::::::::::::::;::::|
+|* * * * * * * * * * OOOOOOOOOOOOOOOOOOOOOOOOO|
+|:::::::::::::::::::::::::::::::::::::::::::::|
+|OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO|
+|:::::::::::::::::::::::::::::::::::::::::::::|
+|OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO|
+|:::::::::::::::::::::::::::::::::::::::::::::|
+|OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO|\n\n"
+
+  cat(flag)
+
+  #
+  #
+}
