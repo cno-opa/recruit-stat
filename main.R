71835125cb0e79f018127d4602e66d60276a7422
Conor <conorsg@gmail.com>
2014-12-12 15:29:16 -0600

    put print calls inside init fn

--- main.R
+++ main.R
@@ -7,14 +7,12 @@ require(xtermStyle)
 init <- function(subdir) {
   Rfiles <- list.files(subdir, pattern = "*.R", full.names = TRUE)
   sapply(Rfiles, source)
+  l <- as.character( ls( globalenv() ) )
+  cat( style( "Loaded the following functions:", fg = 208), style(l, fg = 069), sep = "\n" )
 }
 
 #sequence of script executions
 init("R")
-
-l <- as.character( ls() )
-cat( style( "Loaded the following functions:", fg = 208), style(l, fg = 069), sep = "\n" )
-
 init_clean()
 init_analysis()
 
