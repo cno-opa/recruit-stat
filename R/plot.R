18acfad8abbd5443bf8d76daac0e8fe4c7048e04
Conor <conorsg@gmail.com>
2014-12-16 13:25:10 -0600

    initial commit and relative step yields histogram

--- R/plot.R
+++ R/plot.R
@@ -1,9 +1,10 @@
 #plot.R
 #makes charts from analysis data objects
 
-#TODO: relevel prop table steps in correct order (see: http://www.cookbook-r.com/Manipulating_data/Changing_the_order_of_levels_of_a_factor/)
+#TODO: make a theme
 
 require(ggplot2)
+require(scales)
 require(reshape2)
 
 init_plot <- function() {
@@ -12,11 +13,21 @@ init_plot <- function() {
 
 load("./data/analysis-obj.Rdata")
 
-#relative step success rates
+#data transformation for plotting
+step_titles <- factor( c("Qualified", "Submitted documents", "Scheduled MC", "Attended MC", "Passed MC", "Scheduled WE", "Attended WE", "Passed WE", "Scheduled Agility", "Attended Agility", "Passed Agility"), levels = c("Qualified", "Submitted documents", "Scheduled MC", "Attended MC", "Passed MC", "Scheduled WE", "Attended WE", "Passed WE", "Scheduled Agility", "Attended Agility", "Passed Agility") )
+step_success_prop_table$steps <- step_titles
 
+#relative step success rates
 steps <- melt(step_success_prop_table)
 ggplot(data = steps, aes(x = steps, y = value, fill = variable)) +
-  geom_bar(stat = "identity", position = position_dodge())
+  geom_bar(stat = "identity", position = position_dodge(width = 0.7), width = 0.7) +
+  theme(axis.text.x = element_text(angle = 45, hjust = .97)) +
+  scale_fill_manual( name = "Cohorts", values = c("#FF726B","#82ACDB", "#225A98" ) ) +
+  scale_y_continuous(labels = percent) +
+  labs( title = "Individual step yields", x = "Steps", y = "Success rate" ) +
+  ggsave("./output/rel-steps.png", width = 10, height = 5.5)
+  cat( style( "Saving individual step yields histogram...", fg = 208) )
+
 
 
 #
